4aa7986aab9c362541f8cd329598b487a4e38bfa
gensisblock3301 <nur15-1463@diu.edu.bd>
2025-09-18 11:57:13 +0600

    generate fake data

--- apps/users/serializers/__init__.py
+++ apps/users/serializers/__init__.py
@@ -1,2 +1,17 @@
 from .auth_serializer import AuthenticationSerializer, UserSignupSerializer, UserMeSerializer
 from .password_serializer import VerifyOTPSerializer, SendOtpSerializer, ResetPasswordSerializer
+
+__all__ = [
+    "UserSignupSerializer",
+    "UserMeSerializer",
+    "AuthenticationSerializer",
+    "VerifyOTPSerializer",
+    "SendOtpSerializer",
+    "ResetPasswordSerializer",
+    "UserSignupSerializer",
+    "UserMeSerializer",
+    "AuthenticationSerializer",
+    "VerifyOTPSerializer",
+    "SendOtpSerializer",
+    "ResetPasswordSerializer",
+]
